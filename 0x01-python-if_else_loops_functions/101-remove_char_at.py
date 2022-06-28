6343faeb8a6744c5c23e3af8f878c5d3572e2cb3
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-06-28 22:28:49 +0100

    Fix pycodestyle issues

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,7 +4,7 @@ def remove_char_at(str, n):
     if n < 0 or n >= len(str):
         return str
     for i in range(n):
-        newstr += str[i] # copy char from 0 to n
+        newstr += str[i]  # copy char from 0 to n
     for i in range(n + 1, len(str)):
-        newstr += str[i] # copy from n+1 to end of string
+        newstr += str[i]  # copy from n+1 to end of string
     return newstr
