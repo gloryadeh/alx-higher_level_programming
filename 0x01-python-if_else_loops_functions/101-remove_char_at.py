b5947831bfad7c468262abd63143ca483a2cb39c
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-06-28 22:22:38 +0100

    Solve remaining tasks

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    newstr = ""
+    if n < 0 or n >= len(str):
+        return str
+    for i in range(n):
+        newstr += str[i] # copy char from 0 to n
+    for i in range(n + 1, len(str)):
+        newstr += str[i] # copy from n+1 to end of string
+    return newstr
