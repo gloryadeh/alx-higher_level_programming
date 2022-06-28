ff1142873615b568e1553c02de573fc209bf740f
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-06-28 22:23:43 +0100

    Delete unnecessary things

--- 0x01-python-if_else_loops_functions/102-main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-magic_calculation = __import__('102-magic_calculation').magic_calculation
-
-print("{}".format(magic_calculation(1, 2, 3)))
-print("{}".format(magic_calculation(6, 5, 6)))
