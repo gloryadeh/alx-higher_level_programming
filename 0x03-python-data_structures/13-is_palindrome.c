c3ff6905d5e284b9c8880ac208d42f43764560a0
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-07-04 02:02:43 +0100

    Fix issues

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,34 +1,34 @@
 #include "lists.h"
 
 /**
- * is_palindrome - calls helper function to check if a
- * singly linked list is a palindrome
- * @head: head pointer
- * Return: 0 or 1
+ * is_palindrome - calls helper to check if a singly linked list
+ *                 is a palindrome
+ * @head: head of listint_t linked list
+ *
+ * Return: 0 if not palindrome, 1 if it is
  */
 int is_palindrome(listint_t **head)
 {
-    if (head == NULL || *head == NULL || (*head)->next == NULL)
-        return (1);
-    return (is_pal_helper(head, *head));
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+		return (1);
+	return (is_pal_helper(head, *head));
 }
 
 /**
- * is_pal_helper - checks if a singly linked list is a
- * palindrome
+ * is_pal_helper - checks if a singly linked list is a palindrome
  * @head: head of listint_t linked list
  * @end: pointer to end of palindrome
  *
- *Return 0 if not palindrom, 1 if it is
+ * Return: 0 if not palindrome, 1 if it is
  */
 int is_pal_helper(listint_t **head, listint_t *end)
 {
-    if (end == NULL)
-        return (1);
-    if (is_pal_helper(head, end->next) && (*head)-> == end->n)
-    {
-        *head = (*head)->next;
-        return (1);
-    }
-    return (0);
+	if (end == NULL)
+		return (1);
+	if (is_pal_helper(head, end->next) && (*head)->n == end->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
 }
