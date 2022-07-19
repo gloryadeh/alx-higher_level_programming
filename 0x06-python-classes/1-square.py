ce66683c651ffd2fcf3ed4332f05a748fdf1cd44
gloryadeh <glory.adeh@stu.cu.edu.ng>
2022-07-19 17:52:20 +0100

    Fix issues

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Square with private instance attribute size"""
+
+
 class Square:
+    """Arguments:
+            size: size of Square
+    """
     def __init__(self, size=0):
         self.__size = size
